efd0fc32310cd3d47585230df1a62defe4a5d943
Romain Thoreau <romain.thoreau@outlook.fr>
2017-08-21 16:59:55 +0200

    Add chapters controller and show, new and create methods

---
+++ app/controllers/chapters_controller.rb
@@ -0,0 +1,31 @@
+class ChaptersController < ApplicationController
+
+  before_action :set_book, only: [:new, :create]
+
+  def show
+    @chapter = Chapter.find(params[:id])
+  end
+
+  def new
+    @chapter = Chapter.new
+    @chapter.book = @book
+  end
+
+  def create
+    @chapter = Chapter.new(chapter_params)
+    @chapter.book = @book
+    if @chapter.save
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+
+  def chapter_params
+    params.require(:chapter).permit(:title, :content)
+  end
+
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+end
