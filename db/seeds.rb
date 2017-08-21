ba5dd9f1a50a9b02bd9c04aeb5aef7c16b13bca2
Romain Thoreau <romain.thoreau@outlook.fr>
2017-08-21 17:30:18 +0200

    Create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning seed"
+
+User.destroy_all
+Book.destroy_all
+Chapter.destroy_all
+
+puts "Generating seed"
+
+pierre = User.new ({
+  first_name: 'Pierre',
+  last_name: 'Dutronc',
+  sex: 'Homme',
+  age: 23,
+  description: 'Jeune écrivain talentueux'
+  })
+
+pierre.save
+
+tendre_est_la_nuit = Book.new ({
+  title: 'Tendre est la nuit',
+  summary: "Romance sur la côte d'azur pendant les années folles",
+  category: 'Romance'
+  })
+
+tendre_est_la_nuit.user = pierre
+
+tendre_est_la_nuit.save
+
+chapter_1 = Chapter.new({
+  title: 'Chapter 1'
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  })
+
+chapter_1.book = tendre_est_la_nuit
+
+chapter_1.save
+
+puts "Seed complete"
