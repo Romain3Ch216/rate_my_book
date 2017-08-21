7167b2b8cc13887207ecdb6db7e5f7bacf0dcd9a
Romain Thoreau <romain.thoreau@outlook.fr>
2017-08-21 16:48:04 +0200

    Add show, new, create, edit, update and destroy methods to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
 
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
   def index
     if search_params.present?
       @books = Book.where('category LIKE ?', search_params[:category])
@@ -12,18 +14,33 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+    @book.user = current_user
   end
 
   def create
+    @book = current_user.books.build(book_params)
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @book.destroy
+    redirect_to user_path(current_user)
   end
 
   private
@@ -31,4 +48,13 @@ class BooksController < ApplicationController
   def search_params
     params.require(:search).permit(:category)
   end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :summary, :category)
+  end
+
 end
