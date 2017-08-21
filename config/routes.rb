8adb15538283245371dc796c1016687059fe8499
Romain Thoreau <romain.thoreau@outlook.fr>
2017-08-21 17:02:36 +0200

    Mapping chapters show and new actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :books, only: [:index,:show, :new, :edit]
+  resources :books, only: [:index,:show, :new, :edit] do
+    resources :chapters, only: [:show, :new]
+  end
+
 end
