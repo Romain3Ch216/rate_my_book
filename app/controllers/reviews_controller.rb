02e2b1382db4ff4392a3be627c2ef035be31f514
Romain Thoreau <romain.thoreau@outlook.fr>
2017-08-22 13:26:26 +0200

    Correct the chapter id

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,17 @@
 class ReviewsController < ApplicationController
 
+  before_action :set_chapter, only: [:new, :create]
+
   def new
     @review = Review.new
     @review.user = current_user
-    @review.chapter = Chapter.find(params[:id])
+    @review.chapter = @chapter
   end
 
   def create
     @review = Review.new(review_params)
     @review.user = current_user
-    @review.chapter = Chapter.find(params[:id])
+    @review.chapter = @chapter
     if @review.save
       redirect_to @chapter
     else
@@ -21,5 +23,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:content)
+  end
+
+  def set_chapter
+    @chapter = Chapter.find(params[:chapter_id])
   end
 end
