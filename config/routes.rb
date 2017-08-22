6ca3265eb6efdfcc78727813ce61a89de5287f88
Romain Thoreau <romain.thoreau@outlook.fr>
2017-08-22 11:05:11 +0200

    Mapping reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :books, only: [:index,:show, :new, :edit] do
-    resources :chapters, only: [:show, :new]
+  resources :books, shallow: true, only: [:index,:show, :new, :edit] do
+    resources :chapters, only: [:show, :new] do
+      resources :reviews, only: [:new, :create]
+    end
   end
 
 end
