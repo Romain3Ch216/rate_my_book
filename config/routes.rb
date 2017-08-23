8300379503c8f36203cf9f8e7a061ca9869fea0f
Stephanie Smith <stephaniecatherine.smith@gmail.com>
2017-08-23 12:50:18 +0200

    Book card finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :books, only: [:index,:show, :new, :edit] do
+  resources :books, only: [:index, :show, :new, :edit] do
     resources :chapters, only: [:show, :new]
   end
 
