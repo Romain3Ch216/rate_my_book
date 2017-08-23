0103735838e243e2b50fd7155922118dd8e0ca33
Romain Thoreau <romain.thoreau@outlook.fr>
2017-08-23 16:15:12 +0200

    updating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,6 +55,18 @@ bob = User.new ({
 
 bob.save!
 
+leo = User.new ({
+  first_name: 'Leo',
+  last_name: 'Pacquement',
+  sex: 'Homme',
+  age: 23,
+  description: 'Sosie de Pierre Niney ',
+  email: 'lp@gmail.com',
+  password: '123soleil'
+  })
+
+leo.save!
+
 ecume_chapter_1 = Chapter.new({
   title: 'Chapter 1',
   content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -98,6 +110,7 @@ chapter_3 = Chapter.new({
 chapter_3.book = tendre_est_la_nuit
 chapter_3.read_count = 168
 chapter_3.save!
+
 
 review_1 = Review.new ({
   content: "Super chapitre, j'adore. J'ai hâte de découvrir la suite! orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
