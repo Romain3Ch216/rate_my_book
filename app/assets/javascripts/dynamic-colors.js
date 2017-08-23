a15c62a907775dbcf741e64d3e68d48c05e5c09a
Leopold Pacquement <leopoldpacquement@gmail.com>
2017-08-23 17:14:15 +0200

    color done

--- app/assets/javascripts/dynamic-colors.js
+++ app/assets/javascripts/dynamic-colors.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
 
+  var color = $(".chapter-info").data("color");
+
   $(".chapter-info").hover(function(e) {
-    var color = $(".chapter-info").data("color");
-    $(this).css(
-      "background-color", e.type === "mouseenter"?color.replace(/[^,]+(?=\))/, '0.2'):"transparent",
-    );
+    $(this).css({
+      "background-color": e.type === "mouseenter"?color.replace(/[^,]+(?=\))/, '0.2'):"transparent",
+      "border-left-color": e.type === "mouseenter"?color:"transparent",
+    });
   })
 
+  $(".resume-reading-button").css({
+      "background-color": color,
+      "border-color": color,
+    });
   $(".resume-reading-button").hover(function(e) {
-    var color = $(".resume-reading-button").data("color");
-    $(this).css(
-      "background-color", e.type === "mouseenter"?color:"transparent",
-      "border-left-color", e.type === "mouseenter"?color:"transparent",
-    );
+    $(this).css({
+      "background-color": e.type === "mouseenter"?"transparent":color,
+      "color": e.type === "mouseenter"?color:"white",
+    });
   })
 
 });
