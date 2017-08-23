4ab6745ddd999e7eddd54293d24e30901a775868
Romain Thoreau <romain.thoreau@outlook.fr>
2017-08-23 12:03:45 +0200

    Add edit / update methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
 
   def dashboard
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:description)
+  end
 end
