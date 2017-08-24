6c3a5be4c4f1177c73c57d6713b2a162be96764c
Leopold Pacquement <leopoldpacquement@gmail.com>
2017-08-24 15:53:14 +0200

    work in progress

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,4 +1,8 @@
 class UpvotesController < ApplicationController
+
+
+
+
   def create
     @review = Review.find(params[:review_id])
     @upvote = current_user.upvotes.create(review: @review)
@@ -8,8 +12,21 @@ class UpvotesController < ApplicationController
         format.html { redirect_to book_path(@review.chapter.book, anchor: "modal-open") }
         format.js # render views/reviews/create.js.erb
       end
+    else
+
     end
 
+  end
+
+
+  def destroy
+    @review = Review.find(params[:review_id])
+    @review.destroy
+
+    respond_to do |format|
+        format.html { redirect_to book_path(@review.chapter.book, anchor: "modal-open") }
+        format.js # render views/reviews/create.js.erb
+    end
   end
 
 end
--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,4 +1,5 @@
 class Upvote < ApplicationRecord
   belongs_to :user
   belongs_to :review
+  # validates_uniqueness_of :user_id, :scope => [:review_id]
 end
