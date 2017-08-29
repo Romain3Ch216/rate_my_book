87fd7b4e9f3474910533e1e2656cab8b93d1281c
Leopold Pacquement <leopoldpacquement@gmail.com>
2017-08-29 09:49:05 +0200

    file js deleted

--- app/assets/javascripts/text-area-autogrow.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-  $('.chapter-new-form').on( 'change keyup keydown paste cut', 'textarea', function (){
-      $(this).height(0).height(this.scrollHeight);
-  }).find( 'textarea' ).change();
-});
