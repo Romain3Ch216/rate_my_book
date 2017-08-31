5a1dd51477c655b1f8842ebc5c3a176bf867aa46
Romain Thoreau <romain.thoreau@outlook.fr>
2017-08-31 09:43:24 +0200

    Add style

--- app/assets/javascripts/dynamic-colors.js
+++ app/assets/javascripts/dynamic-colors.js
@@ -10,6 +10,14 @@ $(document).ready(function() {
     });
   })
 
+  $(".blocked").hover(function(e) {
+    $(this).css({
+      "background-color": "rgba(230, 230, 230, 1)",
+      "border-left-color": "rgba(230, 230, 230, 1)",
+    });
+  })
+
+
   // $(".comment-link").hover(function(e) {
   //   $("#comment-icon").attr("fill", e.type === "mouseenter"?"#B2B2B2":"#C0C0C0");
   // });
