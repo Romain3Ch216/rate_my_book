bd3acc84f13fc6d4256f359e09f4e5596ea4eb4d
Romain Thoreau <romain.thoreau@outlook.fr>
2017-08-31 17:55:22 +0200

    Final details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,9 @@ class ApplicationController < ActionController::Base
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+  def default_url_options
+  { host: ENV["HOST"] || "localhost:3000" }
+  end
 
 end
 
