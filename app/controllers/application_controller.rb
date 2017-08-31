45c17dd95a8ff24c7f9b18fa5564c29d524ed3cd
Leopold Pacquement <leopoldpacquement@gmail.com>
2017-08-31 11:27:23 +0200

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { host: ENV["HOST"] || "localhost:3000" }
+    { host: ENV["www.ratemybook.xyz"] || "localhost:3000" }
   end
 end
 
