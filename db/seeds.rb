af19188767b892ff28cb804129c9fc47b3720821
Romain Thoreau <romain.thoreau@outlook.fr>
2017-09-01 14:06:25 +0200

    final seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ pierre = User.new ({
   })
 pierre.save!
 
-url = 'http://res.cloudinary.com/dbojzvt5e/image/upload/v1503912359/s4wfw81yyebjrrs52l17.jpg'
+url = 'http://res.cloudinary.com/dbojzvt5e/image/upload/v1504262186/chinese-man-avatar_e4ihn6.png'
 boris = User.new ({
   first_name: 'Boris',
   last_name: 'Durand',
@@ -59,7 +59,7 @@ leo = User.new ({
   })
 leo.save!
 
-url = 'http://res.cloudinary.com/dbojzvt5e/image/upload/v1504188053/duras_qxfstz.jpg'
+url = 'http://res.cloudinary.com/dbojzvt5e/image/upload/v1504262186/lana-avatar_ynour4.png'
 ida = User.new ({
   first_name: 'Ida',
   last_name: 'Fornebu',
@@ -84,7 +84,7 @@ arnaud = User.new ({
 
 arnaud.save!
 
-url = 'http://res.cloudinary.com/dbojzvt5e/image/upload/v1504188271/natalie_hk1zvz.jpg'
+url = 'http://res.cloudinary.com/dbojzvt5e/image/upload/v1504262186/black-woman-avatar_aldxuf.png'
 lana = User.new ({
   first_name: 'Lana',
   last_name: 'Duras',
@@ -118,7 +118,7 @@ lana.photo_url = url
 
 ecume = Book.new ({
   title: "L'écume des nuits",
-  summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse felis nisi, pretium sit amet mauris vel, bibendum lobortis ante. Maecenas vel fringilla elit. Donec vitae odio quis elit imperdiet bibendum vel et odio. Mauris lacinia nisi non magna suscipit sagittis. Etiam maximus sodales ex, ut tristique lacus feugiat euismod.",
+  summary: "C'est l'histoire d'un jeune homme maladroit, fou amoureux d'une jeune femme maladroite, qui l'invite à danser sur un quart de lune. ",
   category: 'Fantastique'
   })
 ecume.user = boris
@@ -134,7 +134,7 @@ atacama.save!
 
 zorro = Book.new ({
   title: "Zorro",
-  summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse felis nisi, pretium sit amet mauris vel, bibendum lobortis ante. Maecenas vel fringilla elit. Donec vitae odio quis elit imperdiet bibendum vel et odio. Mauris lacinia nisi non magna suscipit sagittis. Etiam maximus sodales ex, ut tristique lacus feugiat euismod.",
+  summary: "Un chevalier des ténèbres, voilé d'un masque noir et d'une cape aussi sombre que la nuit, écorche d'un grand Z les privilèges de la bourgoisie. ",
   category: 'Fantastique'
   })
 zorro.user = arnaud
@@ -144,7 +144,7 @@ zorro.save!
 
 tendre_est_la_nuit = Book.new ({
   title: 'Tendre est la nuit',
-  summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse felis nisi, pretium sit amet mauris vel, bibendum lobortis ante. Maecenas vel fringilla elit. Donec vitae odio quis elit imperdiet bibendum vel et odio. Mauris lacinia nisi non magna suscipit sagittis. Etiam maximus sodales ex, ut tristique lacus feugiat euismod.",
+  summary: "Un été sur la côte d'azur pour Rosemary, jeune étoile montante d'Hollywood, qui rencontre en France Catherine et son mari, de qui elle tombera éperdumment amoureuse.",
   category: 'Romance'
   })
 tendre_est_la_nuit.user = pierre
@@ -152,7 +152,7 @@ tendre_est_la_nuit.save!
 
 coder_ou_respirer = Book.new ({
   title: 'Coder ou respirer',
-  summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse felis nisi, pretium sit amet mauris vel, bibendum lobortis ante. Maecenas vel fringilla elit.",
+  summary: "Sylvain, jeune adolescent passionné par l'informatique, tombe amoureux de sa camarade de classe, Rose. Si certains écrivent des poèmes par amour, lui écrit du code. C'est un code qui vous emménera dans les limbes de Ruby, pour un voyage inoubliable.",
   category: 'Romance'
   })
 coder_ou_respirer.user = leo
@@ -160,7 +160,7 @@ coder_ou_respirer.save!
 
 londres = Book.new ({
   title: "Londres est une fête",
-  summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse felis nisi, pretium sit amet mauris vel, bibendum lobortis ante. Maecenas vel fringilla elit.",
+  summary: "Londres, 1939. Walter rit aux éclats devant la devanture du Mc Laren's. Il est saoul et il est heureux. Demain, il se marie. Le temps est lourd, l'orage gronde.",
   category: 'Romance'
   })
 
@@ -171,15 +171,15 @@ londres.save!
 
 racines = Book.new ({
   title: 'Les Racines de la Terre',
-  summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse felis nisi, pretium sit amet mauris vel, bibendum lobortis ante. Maecenas vel fringilla elit. Donec vitae odio quis elit imperdiet bibendum vel et odio. Mauris lacinia nisi non magna suscipit sagittis. Etiam maximus sodales ex, ut tristique lacus feugiat euismod.",
+  summary: "Quand le crépuscule tombe sur les rives du Batha, il ne reste que les oiseaux pour éventrer les flancs du silence.",
   category: 'Biographie'
   })
 racines.user = ida
 racines.save!
 
 napoleon = Book.new ({
-  title: 'Napoleon, né pour régner',
-  summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse felis nisi, pretium sit amet mauris vel, bibendum lobortis ante. Maecenas vel fringilla elit. Donec vitae odio quis elit imperdiet bibendum vel et odio. Mauris lacinia nisi non magna suscipit sagittis. Etiam maximus sodales ex, ut tristique lacus feugiat euismod.",
+  title: 'Napoleon, la chute du faucon',
+  summary: "Le 8 juin 1777, Charles Bonaparte est élu député de la noblesse de Corse. En cette qualité, il fait partie de la députation que l’Assemblée générale des États de la Corse envoie à Versailles auprès du roi Louis XVI. Le 15 décembre 1778, il part pour Versailles où Louis XVI le reçoit en audience une seconde fois, la première rencontre avec le roi datant de 1776.",
   category: 'Biographie'
   })
 
@@ -188,7 +188,7 @@ napoleon.save!
 
 sylvain = Book.new ({
   title: 'Sylvain, né pour coder',
-  summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse felis nisi, pretium sit amet mauris vel, bibendum lobortis ante. Maecenas vel fringilla elit. Donec vitae odio quis elit imperdiet bibendum vel et odio. Mauris lacinia nisi non magna suscipit sagittis. Etiam maximus sodales ex, ut tristique lacus feugiat euismod.",
+  summary: "Sylvain code plus vite que son ombre. Le tintement du clavier résonne dans sa chambre dans un grand vacarme. Les pupilles dilatées, le front luisant, il programme le monde de demain, muni d'un chocolat chaud à la main.",
   category: 'Biographie'
   })
 
@@ -227,7 +227,7 @@ review_1 = Review.create(user: boris, chapter:atacama_chapter_2, content: 'Lorem
 User.all[0, n -30].each{|user| Upvote.create(user: user, review: review_1)}
 review_2 = Review.create(user: lana, chapter:atacama_chapter_2, content: "Très bon chapitre, je suis absorbée par l'intrigue. Cependant, je ne comprends pas, Carlota n'a fait de mal à personne.")
 User.all[0, n -25].each{|user| Upvote.create(user: user, review: review_2)}
-review_3 = Review.create(user: ida, chapter:atacama_chapter_2, content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'*3)
+review_3 = Review.create(user: ida, chapter:atacama_chapter_2, content: "J'adore le style, l'intrigue est passionnante. L'histoire est vraiment cohérente, je m'identifie très bien au personnage principal. D'ailleurs, j'ai su que Carlota était louche. J'ai demandé la suite sans hésitation en espérant pouvoir vous lire très prochainement.")
 User.all[0, n -13].each{|user| Upvote.create(user: user, review: review_3)}
 
 atacama_chapter_2.save!
@@ -362,7 +362,7 @@ User.all[0,n-23].each{|user| Review.create(user: user, chapter:coder_ou_respirer
 
 coder_ou_respirer_chapter_1.save!
 
-url = 'http://res.cloudinary.com/dbojzvt5e/image/upload/v1504187608/IMG_1479_zs507v.jpg'
+url = 'http://res.cloudinary.com/dbojzvt5e/image/upload/v1504262989/romain-avatar_irpgha.png'
 romain = User.new ({
   first_name: 'Romain',
   last_name: 'Thoreau',
